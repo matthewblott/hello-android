92397eca5b68b44cc308b966ac2902360da1bf94
Matthew Blott <m4ttb@pm.me>
2024-05-22 12:08:39 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+  namespace = "com.example.hello"
+  compileSdk = 34
+  defaultConfig {
+    minSdk = 34
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  // composeOptions {
+  //   kotlinCompilerExtensionVersion = "1.5.1"
+  // }
+}
+
+dependencies {
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.core.ktx)
+}
